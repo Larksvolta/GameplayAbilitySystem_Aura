f6f4af32f99d156857dd1c7853ae5bac5b39da07
Larksvolta <larksvolta@gmail.com>
2025-04-01 20:10:05 +0200

    Overlay Widget Controller

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -15,7 +15,6 @@ AAuraPlayerState::AAuraPlayerState()
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
 	
 	NetUpdateFrequency = 100.f;
-	
 }
 
 UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
